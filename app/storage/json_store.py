a083c1a309df49fe5f426f2985731bebf3d5585c
saikumarseepana <saikumarseepana436@gmail.com>
2026-05-06 20:36:52 +0530

    feat: add load and save methods to JSONStore

--- app/storage/json_store.py
+++ app/storage/json_store.py
@@ -1,7 +1,18 @@
 import json
+import os
 
 class JSONStore:
-    def save(self, data, filename='emails.json'):
-        with open(filename, 'w') as f:
-            json.dump(data, f, indent=4)
+    def __init__(self, filename='emails.json'):
+        self.filename = filename
+
+    def load(self):
+        if not os.path.exists(self.filename):
+            return []
+        with open(self.filename, 'r') as f:
+            return json.load(f)
             
+    def save(self, data):
+
+        with open(self.filename, 'w') as f:
+            json.dump(data, f, indent=4)
+
